946a907504ce27578e2b684068395f875d1bfdd6
Ran Levy <Ran.levy02@post.runi.ac.il>
2026-01-11 19:55:13 +0200

    dd

--- Network.java
+++ Network.java
@@ -30,8 +30,8 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
    public User getUser(String name) {
-    for (int i = 0; i < userCount; i++) {
-        if (users[i].getName().equalsIgnoreCase(name)) {
+   for (int i = 0; i < userCount; i++) {
+        if (users[i].getName().equals(name)) {
             return users[i];
         }
     }
